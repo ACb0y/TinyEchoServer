f1a3a7ec0590384e2f28ab674ad1de119a8739be
ACb0y <511108862@qq.com>
2023-10-27 17:35:21 +0800

    完善coroutine的单元测试用例

--- test/coroutinetest.cpp
+++ test/coroutinetest.cpp
@@ -29,20 +29,18 @@ TEST_CASE(Coroutine_Success) {
   }
   ScheduleClean(schedule);
 }
-/*
- * // 创建协程
-int CoroutineCreate(Schedule& schedule, Entry entry, void* arg, uint32_t priority = 0);
-// 让出执行权，只能在从协程中调用
-void CoroutineYield(Schedule& schedule);
-// 恢复从协程的调用，只能在主协程中调用
-int CoroutineResume(Schedule& schedule);
-// 恢复指定从协程的调用，只能在主协程中调用
-int CoroutineResumeById(Schedule& schedule, int id);
 
-// 协程调度结构体初始化
-int ScheduleInit(Schedule& schedule, int coroutineCnt, int stackSize = 8 * 1024);
-// 判断是否还有协程在运行
-bool ScheduleRunning(Schedule& schedule);
-// 释放调度器
-void ScheduleClean(Schedule& schedule);
- */
+TEST_CASE(Coroutine_Failure) {
+  Schedule schedule;
+  ScheduleInit(schedule, 1);
+  ASSERT_FALSE(ScheduleRunning(schedule));
+  int id = CoroutineCreate(schedule, print, &schedule);
+  ASSERT_EQ(id, 0);
+  id = CoroutineCreate(schedule, print, &schedule);
+  ASSERT_EQ(id, -1);
+  ASSERT_EQ(CoroutineResume(schedule), Success);
+  ASSERT_EQ(CoroutineResume(schedule), Success);
+  ASSERT_EQ(CoroutineResume(schedule), NotRunnable);
+  ASSERT_EQ(CoroutineResumeById(schedule, 0), NotRunnable);
+  ScheduleClean(schedule);
+}
