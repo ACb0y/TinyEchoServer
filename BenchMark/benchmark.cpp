82dd7dbd11036b14108c0f1e113b16ff7a924903
ACb0y <511108862@qq.com>
2023-11-02 19:27:50 +0800

    代码归档

--- BenchMark/benchmark.cpp
+++ BenchMark/benchmark.cpp
@@ -3,11 +3,12 @@
 
 #include "../cmdline.h"
 #include "../epollctl.hpp"
+#include "../timer.h"
 
 using namespace std;
 
 void usage() {
-  cout << "BenchMark -ip 0.0.0.0 -port 1688 -pkt_size 1024 -client_count 200 -run_time 60" << endl;
+  cout << "BenchMark -ip 0.0.0.0 -port 1688 -pkt_size 1024 -client_count 200 -run_time 60 -short_conn" << endl;
   cout << "options:" << endl;
   cout << "    -h,--help                      print usage" << endl;
   cout << "    -ip,--ip                       service listen ip" << endl;
@@ -15,7 +16,12 @@ void usage() {
   cout << "    -pkt_size,--pkt_size           size of send packet, unit is byte" << endl;
   cout << "    -client_count,--client_count   count of client" << endl;
   cout << "    -run_time,--run_time           run time, unit is second" << endl;
+  cout << "    -short_conn                    use short connect" << endl;
   cout << endl;
+}
+
+void finish(void *data) {
+  // TODO
 }
 
 int main(int argc, char *argv[]) {
@@ -24,15 +30,21 @@ int main(int argc, char *argv[]) {
   int64_t pkt_size;
   int64_t client_count;
   int64_t run_time;
+  bool short_conn{false};
   CmdLine::StrOptRequired(&ip, "ip");
   CmdLine::Int64OptRequired(&port, "port");
   CmdLine::Int64OptRequired(&pkt_size, "pkt_size");
   CmdLine::Int64OptRequired(&client_count, "client_count");
   CmdLine::Int64OptRequired(&run_time, "run_time");
+  CmdLine::BoolOpt(&short_conn, "short");
   CmdLine::SetUsage(usage);
   CmdLine::Parse(argc, argv);
+  Timer timer;
+  timer.Register(finish, nullptr, run_time * 1000);
   while (true) {
     // TODO
+    TimerData timer_data;
+    timer.GetLastTimer(timer_data);
   }
   return 0;
 }
